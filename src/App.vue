1147244ac03fda25ac15b50ca935644e81e55c88
Vincent Belanger <vinbel93@gmail.com>
2022-03-18 12:37:37 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Poll from './components/Poll.vue';
+</script>
+
+<template>
+  <Poll />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Poll.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <pre>test</pre>
+</template>
+
+<style scoped></style>
