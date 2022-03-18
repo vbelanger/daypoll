408275c05cc87bb3c940cd179c83f46036475ca0
Vincent Belanger <vinbel93@gmail.com>
2022-03-18 13:29:47 -0400

    Style and stuff

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -23,14 +23,27 @@ export default {
 </script>
 
 <template>
-  <div>
-    <span>{{ command }}</span>
+  <div class="command">
     <svg-icon v-if="loaded" type="mdi" :path="icon" @click="navigator.clipboard.writeText(command)"></svg-icon>
+    <span>{{ command }}</span>
   </div>
+  <div v-if="loaded">Usage: /timepoll {paste command here}</div>
 </template>
 
 <style scoped>
+.command {
+  padding: 4px 0;
+  border-radius: 10px;
+  border: 2px solid lightgrey;
+  margin-bottom: 10px;
+}
+span {
+  margin-right: 10px;
+}
+
 svg {
   cursor: pointer;
+  height: 14px;
+  color: lightskyblue;
 }
 </style>
