4c5003b36f978350db8b8615e5971bb2958640df
Vincent Belanger <vinbel93@gmail.com>
2022-03-21 20:32:56 -0400

    Add tomorrow

--- functions/poll.js
+++ functions/poll.js
@@ -4,14 +4,18 @@ import parse from 'node-html-parser';
 const getHolidays = async (url) => {
   const response = await axios.get(url);
   const root = parse(response.data);
-  return root.querySelectorAll('span.evcal_desc2.evcal_event_title');
+  return root.querySelectorAll('span.evcal_desc2.evcal_event_title').map((e) => e.text);
 };
 
 export const handler = async () => {
-  const holidays = await getHolidays('https://nationaldaycalendar.com/what-day-is-it/');
+  const today = await getHolidays('https://nationaldaycalendar.com/what-is-today/');
+  const tomorrow = await getHolidays('https://nationaldaycalendar.com/tomorrow/');
 
   return {
     statusCode: 200,
-    body: `Today's cringe time: 1h ${holidays.map((a, i) => `answer${i + 1}: ${a.text}`).join(' ')}`,
+    body: JSON.stringify({
+      today,
+      tomorrow,
+    }),
   };
 };
--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -9,14 +9,15 @@ export default {
   },
   data() {
     return {
+      day: 'today',
       loaded: true,
-      command: 'Fetching poll info...',
+      commands: null,
       icon: mdiContentCopy,
     };
   },
   async created() {
     const response = await axios.get('/api/poll');
-    this.command = response.data;
+    this.commands = response.data;
     this.loaded = true;
   },
   methods: {
@@ -24,10 +25,22 @@ export default {
       navigator.clipboard.writeText(this.command);
     },
   },
+  computed: {
+    command() {
+      const prefix = "Today's cringe time: 2h ";
+      return this.commands ? prefix + this.commands[this.day].map((a, i) => `answer${i + 1}: ${a}`).join(' ') : 'Fetching poll info...';
+    },
+  },
 };
 </script>
 
 <template>
+  <div style="text-align: left; margin-left: 30px">
+    <input type="radio" id="today" value="today" v-model="day" />
+    <label for="today">Today</label>
+    <input type="radio" id="tomorrow" value="tomorrow" v-model="day" />
+    <label for="tomorrow">Tomorrow</label>
+  </div>
   <div class="command">
     <svg-icon v-if="loaded" type="mdi" :path="icon" @click="copy"></svg-icon>
     <span>{{ command }}</span>
